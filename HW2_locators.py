41856d66a7bf8fbc9c4bbc16dc3c825df7ece920
AnumAliMemon <memonanum1@gmail.com>
2023-06-12 10:20:37 -0400

    HW2

---
+++ HW2_locators.py
@@ -0,0 +1,21 @@
+"""
+Amazon logo
+driver.find_element(By.XPATH, "//i[@aria-label='Amazon']")
+Email Field
+driver.find_element(By.XPATH, "//input[@type='email']")
+Continue button
+driver.find_element(BY.ID, "continue")
+Need help link
+driver.find_element(BY.XPATH,  "//span[@class='a-expander-prompt']")
+Forgot your password link
+driver.find_element(BY.ID, "auth-fpp-link-bottom")
+Other issues with Sign-In link
+driver.find_element(BY.ID, "ap-other-signin-issues-link")
+Create your Amazon account button
+driver.find_element(BY.ID, "createAccountSubmit")
+*Conditions of use link
+driver.find_element(BY.XPATH, "//a[contains(@href, 'ap_signin_notification_condition_of_use')]")
+*Privacy Notice link
+driver.find_element(BY.XPATH, "//a[contains(@href, 'ap_signin_notification_privacy_notice')]")
+
+"""
